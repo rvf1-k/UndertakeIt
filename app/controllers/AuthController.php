723ccbe89727585a893e41d5eb477670994196db
Dansoru <rafamorenoriquelme2004@gmail.com>
2026-05-11 19:56:50 +0200

    feat: login and logout

--- app/controllers/AuthController.php
+++ app/controllers/AuthController.php
@@ -27,12 +27,67 @@ class AuthController
 
         if ($created) {
 
-            echo "Usuario registrado";
-
+            header("Location: ?page=login");
+            exit;
         } else {
 
             echo "Error";
-
         }
+    }
+
+    public static function login()
+    {
+        $email = trim($_POST['email']);
+        $password = trim($_POST['password']);
+
+        $user = User::findByEmail($email);
+
+        if (!$user) {
+
+            //die("Usuario no encontrado");
+            echo "Usuario no encontrado";
+        }
+
+        $passwordCorrecta = password_verify(
+            $password,
+            $user['password_hash']
+        );
+
+        if (!$passwordCorrecta) {
+
+            //die("Password incorrecta");
+            echo "Password incorrecta";
+        }
+
+        $_SESSION['user_id'] = $user['id'];
+        $_SESSION['username'] = $user['username'];
+
+        header("Location: ?page=home");
+        exit;
+    }
+
+
+
+    public static function logout()
+    {
+        $_SESSION = [];
+
+        if (ini_get("session.use_cookies")) {
+            $params = session_get_cookie_params();
+            setcookie(
+                session_name(),
+                '',
+                time() - 9999,
+                $params["path"],
+                $params["domain"],
+                $params["secure"],
+                $params["httponly"]
+            );
+        }
+
+        session_destroy();
+        
+        header("Location: ?page=home");
+        exit();
     }
 }
