63e4409cec14c004ec4437a08e40675e264cb9a4
Dansoru <rafamorenoriquelme2004@gmail.com>
2026-05-11 18:45:13 +0200

    feat: creating dashboard

---
+++ app/views/pages/register.php
@@ -0,0 +1,22 @@
+<form action="?page=register" method="POST">
+
+    Usuario:
+    <input type="text" name="username" required>
+
+    <br>
+
+    Email:
+    <input type="email" name="email" required>
+
+    <br>
+
+    Contraseña:
+    <input type="password" name="password" required>
+
+    <br>
+
+    <button type="submit">
+        Registrarse
+    </button>
+
+</form>
