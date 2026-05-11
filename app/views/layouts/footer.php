e5282bfd30f03a051f31c919ee068cf34419253c
Dansoru <rafamorenoriquelme2004@gmail.com>
2026-05-11 18:45:13 +0200

    feat: creating dashboard

---
+++ app/views/layouts/footer.php
@@ -0,0 +1,23 @@
+</div>
+<nav>
+    <ul>
+        <li>
+            <a href="index.html"><i class="fa-solid fa-clipboard-list"></i></a>
+        </li>
+        <li>
+            <a href="calendario.html"><i class="fa-solid fa-calendar-days"></i></a>
+        </li>
+        <li>
+            <a id="crear"><i class="fa-solid fa-circle-plus"></i></a>
+        </li>
+        <li>
+            <a href="habitos.html"><i class="fa-solid fa-location-pin"></i></a>
+        </li>
+        <li>
+            <a href="#"><i class="fa-solid fa-gear"></i></a>
+        </li>
+    </ul>
+</nav>
+</body>
+<script type="text/javascript" src="app.js"></script>
+</html>
---
+++ public/index.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once __DIR__ . '/../app/controllers/AuthController.php';
+
+$page = $_GET['page'] ?? 'home';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    switch ($page) {
+
+        case 'register':
+            AuthController::register();
+            break;
+
+        case 'login':
+            AuthController::login();
+            break;
+    }
+}
+
+// Layout
+include_once __DIR__ . '/../app/views/layouts/header.php';
+include_once __DIR__ . '/../app/views/layouts/main.php';
+include_once __DIR__ . '/../app/views/layouts/footer.php';
