b1dcb90d403a459270a5c1a00384389b532879f4
Dansoru <rafamorenoriquelme2004@gmail.com>
2026-05-12 19:33:23 +0200

    chore: install Tailwind

--- app/views/components/navbar.php
+++ app/views/components/navbar.php
@@ -1,1 +1,17 @@
-//TODO: Está ya en el footer, hace falta?
+<div id="navbar">
+    <section>
+        <ul>
+            <li>Hoy</li>
+            <li>Próximos 7 días</li>
+            <li>Mis tareas</li>
+        </ul>
+    </section>
+    <section id="groups-section">
+        <ul>
+            <li>Grupo ejemplo</li>
+            <li>Grupo ejemplo1</li>
+            <li>Grupo ejemplo2</li>
+            <li>Grupo ejemplo3</li>
+        </ul>
+    </section>
+</div>
