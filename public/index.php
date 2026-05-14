2626b6f5773664a3f247c736a6e83cae16e18567
Dansoru <rafamorenoriquelme2004@gmail.com>
2026-05-14 19:27:55 +0200

    hotfix: separate logic from render page and controllers

--- public/index.php
+++ public/index.php
@@ -1,13 +1,17 @@
 <?php
 
 require_once __DIR__ . '/../app/controllers/AuthController.php';
+require_once __DIR__ . '/../app/helpers/auth.php';
 
 session_start();
 
 $page = $_GET['page'] ?? 'home';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    switch ($page) {
+
+    $action = $_POST['action'] ?? null;
+
+    switch ($action) {
 
         case 'register':
             AuthController::register();
