cb5a7aab24803d36995181121f40d70fc2eec154
Dansoru <rafamorenoriquelme2004@gmail.com>
2026-05-21 17:35:26 +0200

    style: format and auto tailwind

--- app/views/layouts/footer.php
+++ app/views/layouts/footer.php
@@ -2,6 +2,7 @@
 
 <?php include_once __DIR__ . '/../components/navbar.php'; ?>
 <?php include_once __DIR__ . '/../components/groups/add-groups.php'; ?>
+<?php include_once __DIR__ . '/../components/task-card.php'; ?>
 
 <nav>
     <ul>
@@ -12,20 +13,24 @@
             <a href="?page=calendario"><i class="fa-solid fa-calendar-days"></i></a>
         </li>
         <li>
-            <a id="crear"><i class="fa-solid fa-circle-plus"></i></a>
+            <button id="add-task" class="link-button">
+                <i class="fa-solid fa-circle-plus"></i>
+            </button>
         </li>
         <li>
             <a href="?page=habitos"><i class="fa-solid fa-location-pin"></i></a>
         </li>
         <!-- //TODO: Temporal hasta tener pagina de ajustes -->
-        <form action="?page=logout" method="POST">
+        <li>
+            <form action="?page=logout" method="POST">
 
-            <button type="submit" class="link-button">
-                <i class="fa-solid fa-gear"></i>
-            </button>
+                <button type="submit" class="link-button">
+                    <i class="fa-solid fa-gear"></i>
+                </button>
 
-            <input type="hidden" name="action" value="logout">
-        </form>
+                <input type="hidden" name="action" value="logout">
+            </form>
+        </li>
     </ul>
 </nav>
 </body>
