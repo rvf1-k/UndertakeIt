223074c8ea9b3a467712dc466daf49f593f92e88
Dansoru <rafamorenoriquelme2004@gmail.com>
2026-05-28 18:26:18 +0200

    refractor: normalice $get id

--- app/views/pages/group/edit-section.php
+++ app/views/pages/group/edit-section.php
@@ -1,6 +1,6 @@
 <section class="flex gap-8 p-8 overflow-x-auto bg-gray-100 min-h-screen">
     <?php
-    $groupId = getGroupId();
+    $groupId = getPathId();
     $sectionId = getSectionId();
     if (!GroupController::watchGroup($groupId) || !GroupController::adminGroup($groupId)) {
         header("Location: ?page=group&id={$groupId}");
